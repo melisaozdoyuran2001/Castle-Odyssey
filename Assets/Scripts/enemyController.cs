220cb10f906d341ce67fbdc7df385edd0365f878
melisaozdoyuran <melisaozdoyuran@hotmail.com>
2023-12-01 19:09:41 -0600

    leveles

---
+++ Assets/Scripts/enemyController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour
+{
+    public float moveDistance = 3.0f; 
+    private float speed = 2.0f;
+    private float direction = 1.0f; 
+    private float moved = 0.0f; 
+    void Update()
+    {
+        float moveThisFrame = speed * Time.deltaTime * direction;
+        moved += Mathf.Abs(moveThisFrame);
+        transform.Translate(moveThisFrame, 0, 0);
+
+        if (moved >= moveDistance)
+        {
+            direction *= -1;
+            moved = 0; 
+            Vector3 newScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z); 
+            transform.localScale = newScale; 
+        }
+    }
+}
